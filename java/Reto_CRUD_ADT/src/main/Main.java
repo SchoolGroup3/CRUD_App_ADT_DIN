1722efb7d21dbbcf17fd58fba479f051bb015f46
kev12s <kev12s@gmail.com>
2025-10-16 12:53:29 +0200

    moving main.java

--- java/Reto_CRUD_ADT/src/main/Main.java
+++ java/Reto_CRUD_ADT/src/main/Main.java
@@ -5,17 +5,34 @@
  */
 package main;
 
+import view.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author 2dami
  */
-public class Main {
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
