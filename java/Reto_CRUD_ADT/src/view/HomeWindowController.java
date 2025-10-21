19ae0c4ca55517472df90ab34863b4446374a291
Jago128 <jagobaetxegarai@gmail.com>
2025-10-21 10:45:11 +0200

    e

--- java/Reto_CRUD_ADT/src/view/HomeWindowController.java
+++ java/Reto_CRUD_ADT/src/view/HomeWindowController.java
@@ -14,6 +14,10 @@ import model.User;
 public class HomeWindowController implements Initializable {
 
     private User user;
+    
+    public void setUser(User newUser){
+        this.user = newUser;
+    }
 
     @FXML
     private Label time;
@@ -29,13 +33,12 @@ public class HomeWindowController implements Initializable {
         LocalTime curTime = LocalTime.now();
 
         if (curTime.isAfter(LocalTime.parse("07:00")) && curTime.isBefore(LocalTime.NOON)) {
-            message = "Good morning User!";
+            message = "Good morning "+user.getUser_name()+"!";
         } else if (curTime.isAfter(LocalTime.NOON) && curTime.isBefore(LocalTime.parse("20:00"))) {
-            message = "Good afternoon User!";
+            message = "Good afternoon "+user.getUser_name()+"!";
         } else {
-            message = "Good night User!";
+            message = "Good night "+user.getUser_name()+"!";
         }
-
         return message;
     }
 
--- java/Reto_CRUD_ADT/src/view/LoginWindowController.java
+++ java/Reto_CRUD_ADT/src/view/LoginWindowController.java
@@ -1,14 +1,43 @@
 package view;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.User;
 
 public class LoginWindowController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    public void openWindow(Event event, User user) {
+        // Add this to controller when merging
+        Stage stage = new Stage();
+        Parent root;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/HomeWindow.fxml"));
+            root = loader.load();
+
+            HomeWindowController contInfo = loader.getController();
+            contInfo.setUser(user);
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
