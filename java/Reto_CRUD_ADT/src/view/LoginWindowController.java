b9740aa1a89f901dc9f097813269623e6670a7bf
2dami <2dami@TartangaLH.eus>
2025-10-21 11:40:02 +0200

    changes on the window code

--- java/Reto_CRUD_ADT/src/view/LoginWindowController.java
+++ java/Reto_CRUD_ADT/src/view/LoginWindowController.java
@@ -3,6 +3,9 @@ package view;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -11,6 +14,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -23,11 +27,19 @@ import view.*;
 public class LoginWindowController implements Initializable {
 
     ImplementsBD im = new ImplementsBD();
+    ActionEvent event;
+    
 
     @FXML
     private TextField usernameTextField;
     @FXML
     private PasswordField passwordTextField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button signupButton;
 
     @FXML
     private void handleLogin() {
@@ -76,9 +88,32 @@ public class LoginWindowController implements Initializable {
         alert.showAndWait();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    @FXML
+    private void manejarBotones(ActionEvent event) throws IOException {
+        Button source = (Button) event.getSource();
+        switch (source.getId()) {
+            case "loginButton":
+                handleLogin();
+                break;
+            case "signupButton":
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("SignUpWindow.fxml"));
+                Parent root =null;
+                root= loader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.show();
+                break;
+        }
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            // TODO
+            manejarBotones(event);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginWindowController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
 }
