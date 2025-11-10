970cc11907afc693745f9ee5a4a593a5b0add757
mikelgaray <garaymikelaritz@gmail.com>
2025-11-10 19:47:12 +0100

    try of makin home test

--- java/Reto_CRUD_ADT/test/Test/HomeWindowControllerTest.java
+++ java/Reto_CRUD_ADT/test/Test/HomeWindowControllerTest.java
@@ -1,83 +1,189 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Test;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import model.User;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import controller.Controller;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.*;
+import javafx.stage.Stage;
+import model.*;
+import org.junit.*;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.api.FxToolkit;
 import static org.junit.Assert.*;
-import view.HomeWindowController;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.base.WindowMatchers.isShowing;
+import view.*;
 
-/**
- *
- * @author USUARIO
- */
-public class HomeWindowControllerTest {
-    
-    public HomeWindowControllerTest() {
+public class HomeWindowControllerTest extends ApplicationTest {
+
+    Stage stage;
+    Controller cont;
+    User testUser;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        User testUser = new User(1, "user@test.com", "Pepe", "1234", 123456789, "pep", "jose","Male" ,"E123456789");
+        // Cargar la ventana Home
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/HomeWindow.fxml"));
+        Parent root = loader.load();
+        HomeWindowController controller = loader.getController();
+        controller.setUser(testUser);
+        controller.timeCheck();
+
+        stage.setScene(new Scene(root));
+        stage.setTitle("Home");
+        stage.show();
+        stage.toFront();
+        this.stage = stage;
+        this.cont = new Controller();
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
+
     @After
-    public void tearDown() {
+    public void tearDown() throws Exception {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
     }
 
-    /**
-     * Test of setUser method, of class HomeWindowController.
-     */
     @Test
-    public void testSetUser() {
-        System.out.println("setUser");
-        User user = null;
-        HomeWindowController instance = new HomeWindowController();
-        instance.setUser(user);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testTimeCheckMethod() throws IOException {
+        HomeWindowController controller = getCurrentController();
+        assertNotNull(controller);
+
+        Label timeLabel = lookup("#time").query();
+        assertNotNull(timeLabel.getText());
+        assertFalse(timeLabel.getText().isEmpty());
+        assertTrue(timeLabel.getText().contains("Pepe"));
     }
 
-    /**
-     * Test of timeCheck method, of class HomeWindowController.
-     */
     @Test
-    public void testTimeCheck() {
-        System.out.println("timeCheck");
-        HomeWindowController instance = new HomeWindowController();
-        instance.timeCheck();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testSettingsIconClick() {
+        ImageView settingsIcon = lookup("#settingsIcon").query();
+        assertNotNull(settingsIcon);
+
+        clickOn("#settingsIcon");
+
+        sleep(1000);
+        try {
+            verifyThat(window("Profile"), isShowing());
+            press(KeyCode.ESCAPE);
+        } catch (Exception e) {
+            try {
+                verifyThat("#profileName", Node::isVisible);
+                press(KeyCode.ESCAPE);
+            } catch (Exception ex) {
+                  // Continuar en caso de error
+            }
+        }
     }
 
-    /**
-     * Test of initialize method, of class HomeWindowController.
-     */
     @Test
-    public void testInitialize() {
-        System.out.println("initialize");
-        URL url = null;
-        ResourceBundle rb = null;
-        HomeWindowController instance = new HomeWindowController();
-        instance.initialize(url, rb);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testLogOutIconClick() {
+        ImageView logOutIcon = lookup("#logOut").query();
+        assertNotNull(logOutIcon);
+
+        clickOn("#logOut");
+
+        sleep(1000);
+        try {
+            verifyThat(window("Log Out"), isShowing());
+            clickOn("Cancel");
+        } catch (Exception e) {
+            // Verificar elementos del popup
+            try {
+                verifyThat("Are you sure you want to log out?", Node::isVisible);
+                clickOn("Cancel");
+            } catch (Exception ex) {
+                // Continuar en caso de error
+            }
+        }
     }
-    
+
+  /*  @Test
+    public void testUserInformationDisplay() {
+        HomeWindowController controller = getCurrentController();
+        assertNotNull(controller.user);
+        assertEquals("Pepe", controller.user.getUser_name());
+        assertEquals("pep", controller.user.getName());
+        assertEquals("jose", controller.user.getSurname());
+
+
+        Label timeLabel = lookup("#time").query();
+        assertTrue(timeLabel.getText().contains("Pepe"));
+    }*/
+
+    @Test
+    public void testWindowTitle() {
+        verifyThat(window("Home"), isShowing());
+    }
+
+   /* @Test
+    public void testNavigationToProfile() throws IOException {
+        // Test de navegación a ventana de perfil
+        HomeWindowController controller = getCurrentController();
+
+        interact(() -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/ProfileWindow.fxml"));
+                Parent root = loader.load();
+                ProfileWindowController profileController = loader.getController();
+                profileController.setUser(testUser);
+
+                Stage profileStage = new Stage();
+                profileStage.setScene(new Scene(root));
+                profileStage.setTitle("Profile");
+                profileStage.show();
+
+                verifyThat(window("Profile"), isShowing());
+
+                // Verificar que el controlador de perfil recibió el usuario
+                assertNotNull(profileController.getUser()); // Asumiendo que existe este método
+                assertEquals("testUser", profileController.getUser().getUser_name());
+
+                profileStage.close();
+            } catch (IOException e) {
+                fail("Error loading Profile window: " + e.getMessage());
+            }
+        });
+    }*/
+
+ /*   @Test
+    public void testLogOutPopupMethod() {
+        // Test del método logOutPopup
+        HomeWindowController controller = getCurrentController();
+
+        interact(() -> {
+            assertDoesNotThrow(() -> {
+                controller.logOutPopup(null);
+            });
+        });
+    }*/
+
+
+   /* @Test
+    public void testControllerInitialization() {
+        // Verificar que el controlador se inicializó correctamente
+        HomeWindowController controller = getCurrentController();
+        assertNotNull(controller);
+        assertNotNull(controller.user);
+        assertEquals("testUser", controller.user.getUser_name());
+    }*/
+
+    private HomeWindowController getCurrentController() {
+        try {
+            Node rootNode = stage.getScene().getRoot();
+            Object controller = rootNode.getProperties().get("controller");
+            if (controller instanceof HomeWindowController) {
+                return (HomeWindowController) controller;
+            }
+        } catch (Exception e) {
+            // Intentar obtener el controlador de otra manera
+        }
+        return null;
+    }
+
+
 }
