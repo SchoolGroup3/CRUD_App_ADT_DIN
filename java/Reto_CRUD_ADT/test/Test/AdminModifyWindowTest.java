feae023f3adb1899eed6590a7b242e344f56c12a
Mosi-HB-2006 <mosi.hickman2006@gmail.com>
2025-11-13 21:20:20 +0100

    Adding modify admin window test

---
+++ java/Reto_CRUD_ADT/test/Test/AdminModifyWindowTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.Admin;
+import org.junit.Test;
+import org.testfx.api.FxAssert;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.control.LabeledMatchers;
+import view.AdminModifyAdminController;
+
+/**
+ *
+ * @author Mosi
+ */
+public class AdminModifyWindowTest extends ApplicationTest {
+
+    private Stage stage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Admin testAdmin = new Admin(1, "user@test.com", "pep", "1234", 123456789, "Pepe",  "jose", "E123456789");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/AdminModifyAdmin.fxml"));
+        Parent root = loader.load();
+
+        AdminModifyAdminController controller = loader.getController();
+        controller.setAdmin(testAdmin);
+
+        stage.setScene(new Scene(root));
+        stage.show();
+        stage.toFront();
+        this.stage = stage;
+    }
+
+    @Test
+    public void emptyFields() {
+        clickOn("#txtFieldPhoneNumber");
+        eraseText(10);
+        clickOn("#btnSave");
+        FxAssert.verifyThat("#lblSavedMessage", LabeledMatchers.hasText("You have to complete all the fields"));
+    }
+
+    @Test
+    public void noChanges() {
+        clickOn("#btnSave");
+        FxAssert.verifyThat("#lblSavedMessage", LabeledMatchers.hasText("No changes detected"));
+    }
+    
+    @Test
+    public void modify() {
+        clickOn("#txtFieldName").write("Pepito");
+
+        clickOn("#btnSave");
+        FxAssert.verifyThat("#lblSavedMessage", LabeledMatchers.hasText("Correctly modified"));
+    }
+}
